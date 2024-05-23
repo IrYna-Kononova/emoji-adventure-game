93b64bcefa71ded8d536712118be4d42c3b16be0
IrYna-Kononova <iryna.kononova@dci-studen.org>
2024-05-23 10:30:06 +0200

    added some small changes

--- main.js
+++ main.js
@@ -59,6 +59,7 @@ function simulateEncounter(character, weatherCondition) {
     switch (weatherCondition) {
         case '☀️':
             console.log(chalk.yellow ('It`s a beautiful sunny day!'));
+            character.recoverHealth(10);
             break;
         case '🌧️':
             console.log(chalk.blue ('It`s raining. You are wet and have a cold'));
@@ -69,8 +70,8 @@ function simulateEncounter(character, weatherCondition) {
             character.reduceHealth(15);
             break;
         case '❄️':
-            console.log(chalk.cyan ('It`s snowing. You feel cold and tired'));
-            character.reduceHealth(20);
+            console.log(chalk.cyan ('It`s snowing. You feel incredible!'));
+            character.recoverHealth(5);
             break;
         case '🌪️':
             console.log(chalk.red ('A tornado is coming your way! Run!'));
@@ -88,14 +89,16 @@ function simulateEncounter(character, weatherCondition) {
 function main() {
     const character = new Character ('Adventurer');
     console.log(chalk.bold ('Your journey begins!'));
+
    while (character.health > 0){
-        const weatherCondition = simulateWeather();
-        readlineSync.question(chalk.green ('Press any key to continue...'))
-        simulateEncounter(character, weatherCondition);
-        if(character.health <= 0){
-            console.log(chalk.red ('Game over! Your journey ends'));
-            break;
-        } 
+    const weatherCondition = simulateWeather();
+    readlineSync.question(chalk.green ('Press any key to continue...'))
+    console.clear();
+    simulateEncounter(character, weatherCondition);
+    if(character.health <= 0){
+        console.log(chalk.red ('Game over! Your journey ends'));
+        break;
+      } 
     }  
    
     if (character.health > 0){
